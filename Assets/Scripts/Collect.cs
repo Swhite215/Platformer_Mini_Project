d3af2eec33974cf12c025e39d5932f0d71b73e53
Spencer White <Spencer.White@carat.com>
2018-02-03 15:46:00 -0500

    First commit

---
+++ Assets/Scripts/Collect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collect : MonoBehaviour {
+	public int points;
+
+	public AudioClip coinSound;
+
+	void OnTriggerEnter2D (Collider2D coll) {
+		AudioSource.PlayClipAtPoint (coinSound, transform.position, 1.0f);
+
+		GameManager.AddScore (points);
+		Object.Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/Death.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour {
+
+	public AudioClip deathClip;
+
+	void OnCollisionEnter2D (Collision2D col) {
+		if (col.gameObject.tag == "Player") {
+			Debug.Log (gameObject.name + "killed player");
+			AudioSource.PlayClipAtPoint (deathClip, transform.position, 1.0f);
+			GameManager.Death ();
+		} else {
+			Object.Destroy (col.gameObject);
+		}
+	}
+}
