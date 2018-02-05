fb92a8888c30e01f93892cd2b57465819a56a032
Spencer White <Spencer.White@carat.com>
2018-02-05 18:07:04 -0500

    Updated platform for Level Two

--- Assets/Scripts/Switch.cs
+++ Assets/Scripts/Switch.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Switch : MonoBehaviour {
 
 	public Animator animator;
+
+	public Animator platformAnimator;
+
 	// Use this for initialization
 	void Start () {
 		animator = GetComponent<Animator> ();
@@ -18,6 +21,9 @@ public class Switch : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D coll) {
 		if (coll.gameObject.tag == "Player") {
 			animator.SetTrigger("Switch");
+			if (platformAnimator != null) {
+				platformAnimator.SetTrigger ("Move");
+			}
 		}
 	}
 }
